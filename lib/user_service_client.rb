37fba90fdafa0d44cc363963a1d36247b117df67
Jan Koszewski <jan.koszewski@gmail.com>
2012-06-07 13:30:57 -0400

    added get_user_info method

---
+++ lib/user_service_client.rb
@@ -0,0 +1,37 @@
+require "faraday"
+require "json"
+require "ostruct"
+require_relative 'response'
+
+module UserService
+  class Client
+    attr_accessor :conn
+    attr_accessor :path_prefix
+
+    def initialize(options={})
+      options[:url] ||= "http://localhost:3000"
+      @conn = Faraday.new(:url => options[:url])
+    
+      if options[:path_prefix]
+        @path_prefix = options[:path_prefix]
+      else
+        @path_prefix = ""
+      end
+
+      if token = options[:token]
+        @token = token
+      end
+      @options = options
+    end
+
+    def get_user_info
+      resp = @conn.get do |req|
+        req.url "#{@path_prefix}/api/v1/users.json"
+        req.params["token"] = @token
+        req.headers['Content-Type'] = 'application/json'
+      end
+
+      response = UserService::Response.new(resp.status, OpenStruct.new(JSON.parse(resp.body)))
+    end
+  end
+end
